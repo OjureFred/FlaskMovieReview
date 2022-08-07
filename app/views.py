db784b19cad966498c0e34353ca01478c6033790
Fredrick Ojure <fredojure@gmail.com>
2022-08-07 16:37:52 +0300

    Added secret key and files for movie API

--- app/views.py
+++ app/views.py
@@ -7,12 +7,14 @@ def index():
     '''
     View root page that returns the index page and it's content
     '''
-    message = 'Hello World from Fred Ojure!!!!!'
-    return render_template('index.html', message = message)
+    
+    title = 'Home - Welcome to the Best Movie Reviews Website'
+    return render_template('index.html', title = title)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
     '''
     View detail of a movie gien a movie id
     '''
+    
     return render_template('movie.html', id=movie_id)
